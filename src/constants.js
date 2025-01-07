5516a8830ce4d1d3cdedb20e540dbcb669c3b154
snus4me <mathias.hun@gmail.com>
2025-01-07 18:05:26 +0100

    init project

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  turquise: 'hsl(179deg, 80%, 58%)',
+  mediumBlue: 'hsl(206deg, 84%, 22%)',
+  grey: 'hsl(225deg, 14%, 53%)',
+  white: 'hsl(0deg, 0%, 100%)',
+  darkBlue: 'hsl(230deg, 60%, 8%)',
+  pink: 'hsl(343deg, 100%, 58%)',
+};
