b24f8209d5ee3065eb97c3a3aa8a1d6c160209c8
snus4me <mathias.hun@gmail.com>
2025-01-09 21:47:11 +0100

    coded the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,93 @@
-import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { COLORS } from '../constants';
+import footerSvg from '../assets/bg-footer-squiggle.svg';
+
+import InputField from './InputField';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [inputText, setInputText] = useState(null);
+  const [errorMessage, setErrorMsg] = useState(null);
+
+  const validateEmail = () => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const result = emailPattern.test(inputText);
+
+    if (!result) {
+      return setErrorMsg(`Oops! That doesn't look like an email address!`);
+    }
+    setErrorMsg(null);
+  };
+
+  return (
+    <FooterWrapper>
+      <ImageWrapper>
+        <Image src={footerSvg} alt="" />
+      </ImageWrapper>
+      <InputSection>
+        <BigText>Get notified when we launch</BigText>
+        <InputWrapper>
+          <InputField setInputText={setInputText} errorMessage={errorMessage} />
+          <Button onClick={validateEmail}>Get notified</Button>
+        </InputWrapper>
+      </InputSection>
+    </FooterWrapper>
+  );
 };
+
+const FooterWrapper = styled.footer`
+  height: 228px;
+  margin-top: 150px;
+  margin-bottom: 120px;
+  position: relative;
+  width: 1600px;
+`;
+
+const ImageWrapper = styled.div``;
+
+const Image = styled.img`
+  width: 100%;
+  display: block;
+`;
+
+const InputSection = styled.div`
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  margin-top: 70px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
+
+const BigText = styled.h1`
+  font-size: ${32 / 16}rem;
+  text-align: center;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+const Button = styled.button`
+  width: 140px;
+  height: 48px;
+  border-radius: 24px;
+  background: ${COLORS.turquise};
+  font-size: ${15 / 16}rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background: ${COLORS.darkBlue};
+    border: 2px solid ${COLORS.turquise};
+    color: white;
+  }
+`;
 
 export default Footer;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { COLORS } from '../constants';
+
+const InputField = ({ setInputText, errorMessage }) => {
+  const style = {
+    border: errorMessage ? `2px solid ${COLORS.pink}` : 'none',
+  };
+
+  return (
+    <Wrapper>
+      <Input
+        type="email"
+        name=""
+        id=""
+        placeholder="Email address"
+        onChange={(e) => setInputText(e.target.value)}
+        style={style}
+      />
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  width: 320px;
+  height: 48px;
+  border: none;
+  border-radius: 24px;
+  background: ${COLORS.mediumBlue};
+  font-weight: bold;
+  font-size: ${15 / 16}rem;
+  padding-left: 20px;
+  color: white;
+`;
+
+const ErrorMessage = styled.p`
+  white-space: nowrap;
+  position: absolute;
+  bottom: -26px;
+  left: 20px;
+  font-size: ${12 / 16}rem;
+  color: ${COLORS.pink};
+`;
+
+export default InputField;
